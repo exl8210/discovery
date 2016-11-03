0e8377c74fe7a3d0ba179265507d37989cd165e8
Abbey lee <LLL@Apples-MacBook-Pro-2.local>
2016-11-02 23:33:53 -0400

    slide out nav works!

---
+++ js/ui.js
@@ -0,0 +1,14 @@
+/*
+ui.js
+For the nav slide out menu, and any other ui controls
+*/
+
+"use strict";
+
+function slideOut(){
+    document.getElementById("sideNav).style.width = "100%";
+}
+
+function slideBack(){
+    document.getElementById("sideNav").style.width = "0";
+}
